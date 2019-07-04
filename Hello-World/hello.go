eb777dcd468c944a1af4956e602080e73cf5e4da
Tinashe Wilbrod Chipomho <tinashe@siatik.com>
2019-07-04 13:21:45 +0200

    Refactored testing code

--- Hello-World/hello.go
+++ Hello-World/hello.go
@@ -2,8 +2,14 @@ package main
 
 import "fmt"
 
+//Constants should improve performance of your application
+const englishHelloPrefix = "Hello, "
+
 func Hello(name string) string { //this returns a string
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
--- Hello-World/hello_test.go
+++ Hello-World/hello_test.go
@@ -10,11 +10,26 @@ The test function must start with the word Test
 The test function takes one argument only t *testing.T
 **/
 func TestHello(t *testing.T) {
-	got := Hello("Chris") //We're declaring some variables with the syntax varName := value, which lets us re-use
-	//some values in our test for readability.
-	want := "Hello, Chris"
 
-	if got != want {
-		t.Errorf("got '%s' want '%s'", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		//needed to tell the test suite that this method is a helper
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris") //We're declaring some variables with the syntax varName := value, which lets us re-use
+		//some values in our test for readability.
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
 }
